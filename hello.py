6f1373e5a69158f9f56760bdc1f2ae6ced2ce772
Hyper-TH <twila.shayne.habab@outlook.com>
2022-12-12 21:17:01 +0000

    Modified port

--- hello.py
+++ hello.py
@@ -85,5 +85,5 @@ def read(): # Name of the method
     return ret
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0',port='8080') #Run the flask app at port 8080
-    app.run(port=int(os.environ.get("PORT", 80)),host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0',port='8080') #Run the flask app at port 8080
+    # app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
